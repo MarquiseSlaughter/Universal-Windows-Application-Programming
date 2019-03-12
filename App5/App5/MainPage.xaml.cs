0ed51753ffdd112d149912e870e25cc25a7b2a9e
Marquise Slaughter <marquise.slaughter@smail.rasmussen.edu>
2019-03-12 05:40:56 -0500

    added files
    added files

---
+++ App5/App5/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App5
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ChangeHeader();
+        }
+
+        public void ChangeHeader()
+        {
+            // txtBoxRas.Text = "Rasmussen College";
+            txtBoxRas.FontSize = 14;
+            txtBoxRas.Foreground = new SolidColorBrush(Windows.UI.Colors.Red);
+
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessCalc();
+        }
+
+
+        private void ProcessCalc()
+        {
+            Int32 Var1 = Convert.ToInt32(txtBoxInput1.Text) + Convert.ToInt32(txtBoxInput2.Text);
+            txtBoxDisplay.Text = Convert.ToString(Var1);
+        }
+
+    }
+
+    }
